f2e43bf4f750c4bf3f18a456940631cd363b6e35
DravenPorschE <dravenporsch@gmai.com>
2025-11-23 00:01:29 +0800

    Convert

--- secai_main.py
+++ secai_main.py
@@ -1,46 +1,14 @@
-#!/usr/bin/env python3
-import time
-import os
+from PIL import Image
+import numpy as np
 
-# Framebuffer path
-fb = "/dev/fb1"
+img = Image.open("tin_available.png").convert("RGB")  # drop alpha
+img = img.resize((480, 320))  # optional scale
 
-# Your LCD resolution
-WIDTH = 480
-HEIGHT = 320
+arr = np.array(img)
+r = (arr[:,:,0] >> 3).astype(np.uint16)
+g = (arr[:,:,1] >> 2).astype(np.uint16)
+b = (arr[:,:,2] >> 3).astype(np.uint16)
+rgb565 = (r << 11) | (g << 5) | b
 
-# Helper: write a single RGB565 color to the entire screen
-def fill_screen(color_565):
-    with open(fb, "wb") as f:
-        # RGB565 is 2 bytes per pixel
-        data = color_565.to_bytes(2, byteorder="little")
-        f.write(data * (WIDTH * HEIGHT))
-
-# Standard RGB565 colors
-RED    = 0xF800
-GREEN  = 0x07E0
-BLUE   = 0x001F
-WHITE  = 0xFFFF
-BLACK  = 0x0000
-
-print("Filling RED")
-fill_screen(RED)
-time.sleep(2)
-
-print("Filling GREEN")
-fill_screen(GREEN)
-time.sleep(2)
-
-print("Filling BLUE")
-fill_screen(BLUE)
-time.sleep(2)
-
-print("Filling WHITE")
-fill_screen(WHITE)
-time.sleep(2)
-
-print("Filling BLACK")
-fill_screen(BLACK)
-time.sleep(2)
-
-print("Done.")
+with open("/dev/fb1", "wb") as f:
+    f.write(rgb565.tobytes())
